ccf306c90d01dbad4092a215bbdefde3f2ed1708
Bruno Lima <brunoluislima19@gmail.com>
2025-04-27 17:00:32 -0300

    feat: addiing sddm and configs

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -2,27 +2,32 @@
 set -e
 
 packages=(
-    hyprland
-    waybar
-    wofi
-    kitty
-    neovim
-    fish
-    tmux
-    swaylock
-    wlogout
-    dolphin
-    btop
-    fzf
-    ripgrep
-    fd
-    xdg-desktop-portal-hyprland
-    xdg-desktop-portal
-    eza
+  hyprland
+  waybar
+  wofi
+  kitty
+  neovim
+  fish
+  tmux
+  swaylock
+  wlogout
+  dolphin
+  btop
+  fzf
+  ripgrep
+  fd
+  xdg-desktop-portal-hyprland
+  xdg-desktop-portal
+  eza
+  sddm
 )
 
 echo "==> Installing packages..."
 sudo pacman -S --needed --noconfirm "${packages[@]}"
+
+echo "==> Enabling SDDM..."
+sudo systemctl enable sddm.service --now
+echo "==> SDDM enabled successfully!"
 
 # Após instalar os pacotes, chamar o script de plugins do Fish
 echo "==> Now installing Fish plugins and configuring Fish shell..."
--- sync_configs.sh
+++ sync_configs.sh
@@ -5,16 +5,17 @@ set -e
 DOTFILES_DIR="$(pwd)"
 CONFIGS_DIR="$DOTFILES_DIR/config"
 CONFIGS_TO_SYNC=(
-    btop
-    fish
-    hypr
-    kitty
-    nvim
-    tmux
-    swaylock
-    waybar
-    wlogout
-    wofi
+  btop
+  fish
+  hypr
+  kitty
+  nvim
+  tmux
+  swaylock
+  waybar
+  wlogout
+  wofi
+  sddm
 )
 
 echo "==> Syncing configuration files..."
@@ -24,13 +25,17 @@ mkdir -p "$CONFIGS_DIR"
 
 # Loop through each config and sync it
 for config in "${CONFIGS_TO_SYNC[@]}"; do
-    if [ -d "$HOME/.config/$config" ]; then
-        echo "Copying $config..."
-        rm -rf "$CONFIGS_DIR/$config" # Clean previous version
-        cp -r "$HOME/.config/$config" "$CONFIGS_DIR/"
-    else
-        echo "Warning: $config not found in ~/.config, skipping..."
-    fi
+  if [ -d "$HOME/.config/$config" ]; then
+    echo "Copying $config..."
+    rm -rf "$CONFIGS_DIR/$config" # Clean previous version
+    cp -r "$HOME/.config/$config" "$CONFIGS_DIR/"
+  else
+    echo "Warning: $config not found in ~/.config, skipping..."
+  fi
 done
+
+echo "==> Syncing SDDM configurations..."
+cp -r ~/.config/sddm/* /etc/sddm.conf.d/
+echo "==> SDDM configurations synced!"
 
 echo "==> Configs synced and pushed successfully!"
