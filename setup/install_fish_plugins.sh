4bc69955a72d054bbd0899de46e676054c3572bc
Bruno Lima <brunoluislima19@gmail.com>
2025-06-27 00:20:42 -0300

    fix fish and tide plugin

--- setup/install_fish_plugins.sh
+++ setup/install_fish_plugins.sh
@@ -2,25 +2,27 @@
 set -e
 
 # Verificar se o Fish está instalado
-if ! command -v fish &> /dev/null
-then
-    echo "==> Fish shell is not installed. Please install Fish first."
-    exit 1
+if ! command -v fish &>/dev/null; then
+  echo "==> Fish shell is not installed. Please install Fish first."
+  exit 1
 else
-    echo "==> Fish shell found, proceeding with plugin installation..."
+  echo "==> Fish shell found, proceeding with plugin installation..."
 fi
 
 # Instalar o Fish plugin manager - Fisher, caso não esteja instalado
-if ! command -v fisher &> /dev/null
-then
-    echo "==> Installing Fisher plugin manager..."
-    curl -sL https://git.io/fisher | fish
+if ! command -v fisher &>/dev/null; then
+  echo "==> Installing Fisher plugin manager..."
+  fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
 else
-    echo "==> Fisher is already installed."
+  echo "==> Fisher is already installed."
 fi
 
 # Instalar o tema Tide para o Fish
 echo "==> Installing Tide theme for Fish..."
-fisher install ilancosman/tide
+fish -c 'fisher install ilancosman/tide'
 
-echo "==> Fish plugins installed successfully!"
+# deixar o fish com shell defeult
+command -v fish | sudo tee -a /etc/shells
+chsh -s "$(command -v fish)"
+
+echo "==> Fish plugins installed and Fish set as default sehll successfully!"
